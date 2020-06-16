3a532017daef8d245d1db2bb1d6858d1bfd39c10
Vivek Gopalan <vivek.gopalan@yale.edu>
2020-06-16 01:22:34 -0700

    tablegroup.c

--- src/postgres/src/include/commands/tablegroup.c
+++ src/postgres/src/include/commands/tablegroup.c
@@ -86,97 +86,32 @@ char	   *default_tablegroup = NULL;
 Oid
 CreateTableGroup(CreateTableGroupStmt *stmt)
 {
-#ifdef HAVE_SYMLINK
 	Relation	rel;
-	Datum		values[Natts_pg_tablespace];
-	bool		nulls[Natts_pg_tablespace];
+	Datum		values[Natts_pg_tablegroup];
+	bool		nulls[Natts_pg_tablegroup];
 	HeapTuple	tuple;
-	Oid			tablespaceoid;
-	char	   *location;
-	Oid			ownerId;
-	Datum		newOptions;
+	Oid			tablegroupoid;
 
-	/* Must be super user */
-	if (!superuser())
-		ereport(ERROR,
-				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-				 errmsg("permission denied to create tablespace \"%s\"",
-						stmt->tablespacename),
-				 errhint("Must be superuser to create a tablespace.")));
-
-	/* However, the eventual owner of the tablespace need not be */
-	if (stmt->owner)
-		ownerId = get_rolespec_oid(stmt->owner, false);
-	else
-		ownerId = GetUserId();
-
-	/* Unix-ify the offered path, and strip any trailing slashes */
-	location = pstrdup(stmt->location);
-	canonicalize_path(location);
-
-	/* disallow quotes, else CREATE DATABASE would be at risk */
-	if (strchr(location, '\''))
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_NAME),
-				 errmsg("tablespace location cannot contain single quotes")));
+	// TODO: Any need to check permissions?
+	// TODO: Any need to have a tablegroup owner?
 
 	/*
-	 * Allowing relative paths seems risky
-	 *
-	 * this also helps us ensure that location is not empty or whitespace
-	 */
-	if (!is_absolute_path(location))
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_OBJECT_DEFINITION),
-				 errmsg("tablespace location must be an absolute path")));
-
-	/*
-	 * Check that location isn't too long. Remember that we're going to append
-	 * 'PG_XXX/<dboid>/<relid>_<fork>.<nnn>'.  FYI, we never actually
-	 * reference the whole path here, but MakePGDirectory() uses the first two
-	 * parts.
-	 */
-	if (strlen(location) + 1 + strlen(TABLESPACE_VERSION_DIRECTORY) + 1 +
-		OIDCHARS + 1 + OIDCHARS + 1 + FORKNAMECHARS + 1 + OIDCHARS > MAXPGPATH)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_OBJECT_DEFINITION),
-				 errmsg("tablespace location \"%s\" is too long",
-						location)));
-
-	/* Warn if the tablespace is in the data directory. */
-	if (path_is_prefix_of_path(DataDir, location))
-		ereport(WARNING,
-				(errcode(ERRCODE_INVALID_OBJECT_DEFINITION),
-				 errmsg("tablespace location should not be inside the data directory")));
-
-	/*
-	 * Disallow creation of tablespaces named "pg_xxx"; we reserve this
-	 * namespace for system purposes.
-	 */
-	if (!allowSystemTableMods && IsReservedName(stmt->tablespacename))
-		ereport(ERROR,
-				(errcode(ERRCODE_RESERVED_NAME),
-				 errmsg("unacceptable tablespace name \"%s\"",
-						stmt->tablespacename),
-				 errdetail("The prefix \"pg_\" is reserved for system tablespaces.")));
-
-	/*
-	 * Check that there is no other tablespace by this name.  (The unique
+	 * Check that there is no other tablegroup by this name.  (The unique
 	 * index would catch this anyway, but might as well give a friendlier
 	 * message.)
 	 */
-	if (OidIsValid(get_tablespace_oid(stmt->tablespacename, true)))
+	if (OidIsValid(get_tablegroup_oid(stmt->tablegroupname, true)))
 		ereport(ERROR,
 				(errcode(ERRCODE_DUPLICATE_OBJECT),
 				 errmsg("tablespace \"%s\" already exists",
 						stmt->tablespacename)));
 
 	/*
-	 * Insert tuple into pg_tablespace.  The purpose of doing this first is to
-	 * lock the proposed tablename against other would-be creators. The
+	 * Insert tuple into pg_tablegroup.  The purpose of doing this first is to
+	 * lock the proposed tablegroupname against other would-be creators. The
 	 * insertion will roll back if we find problems below.
 	 */
-	rel = heap_open(TableSpaceRelationId, RowExclusiveLock);
+	rel = heap_open(TableGroupRelationId, RowExclusiveLock);
 
 	MemSet(nulls, false, sizeof(nulls));
 
@@ -202,13 +137,8 @@ CreateTableGroup(CreateTableGroupStmt *stmt)
 
 	heap_freetuple(tuple);
 
-	/* Record dependency on owner */
-	recordDependencyOnOwner(TableSpaceRelationId, tablespaceoid, ownerId);
-
 	/* Post creation hook for new tablespace */
 	InvokeObjectPostCreateHook(TableSpaceRelationId, tablespaceoid, 0);
-
-	create_tablespace_directories(location, tablespaceoid);
 
 	/* Record the filesystem change in XLOG */
 	{
@@ -224,26 +154,11 @@ CreateTableGroup(CreateTableGroupStmt *stmt)
 		(void) XLogInsert(RM_TBLSPC_ID, XLOG_TBLSPC_CREATE);
 	}
 
-	/*
-	 * Force synchronous commit, to minimize the window between creating the
-	 * symlink on-disk and marking the transaction committed.  It's not great
-	 * that there is any window at all, but definitely we don't want to make
-	 * it larger than necessary.
-	 */
-	ForceSyncCommit();
-
-	pfree(location);
 
 	/* We keep the lock on pg_tablespace until commit */
 	heap_close(rel, NoLock);
 
 	return tablespaceoid;
-#else							/* !HAVE_SYMLINK */
-	ereport(ERROR,
-			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-			 errmsg("tablespaces are not supported on this platform")));
-	return InvalidOid;			/* keep compiler quiet */
-#endif							/* HAVE_SYMLINK */
 }
 
 /*
@@ -254,7 +169,6 @@ CreateTableGroup(CreateTableGroupStmt *stmt)
 void
 DropTableSpace(DropTableSpaceStmt *stmt)
 {
-#ifdef HAVE_SYMLINK
 	char	   *tablespacename = stmt->tablespacename;
 	HeapScanDesc scandesc;
 	Relation	rel;
@@ -400,11 +314,6 @@ DropTableSpace(DropTableSpaceStmt *stmt)
 
 	/* We keep the lock on pg_tablespace until commit */
 	heap_close(rel, NoLock);
-#else							/* !HAVE_SYMLINK */
-	ereport(ERROR,
-			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-			 errmsg("tablespaces are not supported on this platform")));
-#endif							/* HAVE_SYMLINK */
 }
 
 /*
